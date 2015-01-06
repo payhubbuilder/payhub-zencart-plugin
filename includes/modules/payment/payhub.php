306e5acf329edade76b39491f80215069e573138
Lon Sun <lsun@payhub.com>
2015-01-06 12:56:15 -0800

    EE-184: Fixed processing issue on zen cart. Also disabled autocomplete on sensitive input fields.

--- includes/modules/payment/payhub.php
+++ includes/modules/payment/payhub.php
@@ -60,13 +60,13 @@ class payhub{
 	                                                 'field' => zen_draw_input_field('payhub_cc_owner', $order->billing['firstname'] . ' ' . $order->billing['lastname'], 'id="'.$this->code.'-cc-owner"'. $onFocus),
 	                                                 'tag' => $this->code.'-cc-owner'),
 	                                           array('title' => MODULE_PAYMENT_PAYHUB_TEXT_CREDIT_CARD_NUMBER,
-	                                                 'field' => zen_draw_input_field('payhub_cc_number', '', 'id="'.$this->code.'-cc-number"' . $onFocus),
+	                                                 'field' => zen_draw_input_field('payhub_cc_number', '', 'id="'.$this->code.'-cc-number"' . $onFocus . " autocomplete=\"off\""),
 	                                                 'tag' => $this->code.'-cc-number'),
 	                                           array('title' => MODULE_PAYMENT_PAYHUB_TEXT_CREDIT_CARD_EXPIRES,
 	                                                 'field' => zen_draw_pull_down_menu('payhub_cc_expires_month', $expires_month, '', 'id="'.$this->code.'-cc-expires-month"' . $onFocus) . '&nbsp;' . zen_draw_pull_down_menu('payhub_cc_expires_year', $expires_year, '', 'id="'.$this->code.'-cc-expires-year"' . $onFocus),
 	                                                 'tag' => $this->code.'-cc-expires-month'),
 	                                           array('title' => MODULE_PAYMENT_PAYHUB_TEXT_CVV,
-	                                                 'field' => zen_draw_input_field('payhub_cc_cvv','', 'size="4", maxlength="4"' . ' id="'.$this->code.'-cc-cvv"' . $onFocus) . ' ' . '<a href="javascript:popupWindow(\'' . zen_href_link(FILENAME_POPUP_CVV_HELP) . '\')">' . MODULE_PAYMENT_PAYHUB_TEXT_POPUP_CVV_LINK . '</a>',
+	                                                 'field' => zen_draw_input_field('payhub_cc_cvv','', 'size="4", maxlength="4"' . ' id="'.$this->code.'-cc-cvv"' . $onFocus . " autocomplete=\"off\"") . ' ' . '<a href="javascript:popupWindow(\'' . zen_href_link(FILENAME_POPUP_CVV_HELP) . '\')">' . MODULE_PAYMENT_PAYHUB_TEXT_POPUP_CVV_LINK . '</a>',
 	                                                 'tag' => $this->code.'-cc-cvv')
 				 ));
 	    } else {
@@ -251,52 +251,54 @@ class payhub{
     $last_order_id = $db->Execute("select * from " . TABLE_ORDERS . " order by orders_id desc limit 1");
     $new_order_id = $last_order_id->fields['orders_id'];
     $new_order_id = ($new_order_id + 1);
+    $new_order_id = "zencart_" . (string)$new_order_id . '-' . zen_create_random_value(4);
 
+    //This overrides the admin setting. Leave for now since you shouldn't be able to change it anyways.
     $url = 'https://vtp1.payhub.com/payhubvtws/transaction.json';
 
     $data = array(
     	"RECORD_FORMAT" => "CC",
     	"CARDHOLDER_ID_CODE" => "@",
     	"CARDHOLDER_ID_DATA" => "",
-			"MERCHANT_NUMBER" => MODULE_PAYMENT_PAYHUB_ORGID,
-			"USER_NAME" => MODULE_PAYMENT_PAYHUB_USERNAME,
-			"PASSWORD" => MODULE_PAYMENT_PAYHUB_PASSWORD,
-			"TERMINAL_NUMBER" => MODULE_PAYMENT_PAYHUB_TERMID,
-			"TRANSACTION_CODE" => "01",
-			"ACCOUNT_DATA_SOURCE" => "T",
-			"CUSTOMER_DATA_FIELD" => $_POST['cc_number'],
-			"CARD_EXPIRY_DATE" => $_POST['cc_expires'],
-			"TRANSACTION_NOTE" => "IP: " . zen_get_ip_address() . "Purchased from: " . STORE_NAME . ", Invoice ID: " . $new_order_id, 
-			"CVV_DATA" => $_POST['cc_cvv'],
-			"CVV_CODE" => ($_POST['cc_cvv'] != NULL) ? "Y" : "N",
-			"TRANSACTION_AMOUNT" => (number_format($order->info['total'], 2) * 100),
-			"OFFLINE_APPROVAL_CODE" => "",
-			"TRANSACTION_ID" => "",
-			"CUSTOMER_ID" => "",
-			"CUSTOMER_FIRST_NAME" => $order->billing['firstname'],
-			"CUSTOMER_LAST_NAME" => $order->billing['lastname'],
-			"CUSTOMER_COMPANY_NAME" => $order->billing['company'],
-			"CUSTOMER_JOB_TITLE" => "",
-			"CUSTOMER_EMAIL_ID" => $order->customer['email_address'],
-			"CUSTOMER_WEB" => "",
-			"CUSTOMER_PHONE_NUMBER" => $order->customer['telephone'],
-			"CUSTOMER_PHONE_EXT" => "",
-			"CUSTOMER_PHONE_TYPE" => "",
-			"CUSTOMER_BILLING_ADDRESS1" => $order->billing['street_address'],
-			"CUSTOMER_BILLING_ADDRESS2" => "",
-			"CUSTOMER_BILLING_ADD_CITY" => $order->billing['city'],
-			"CUSTOMER_BILLING_ADD_STATE" => $state,
-			"CUSTOMER_BILLING_ADD_ZIP" => $order->billing['postcode'],
-			"CUSTOMER_SHIPPING_ADD_NAME" => $order->delivery['firstname'] . $order->delivery['lastname'],
-			"CUSTOMER_SHIPPING_ADDRESS1" => $order->delivery['street_address'],
-			"CUSTOMER_SHIPPING_ADDRESS2" => "",
-			"CUSTOMER_SHIPPING_ADD_CITY" => $order->delivery['city'],
-			"CUSTOMER_SHIPPING_ADD_STATE" => $state,
-			"CUSTOMER_SHIPPING_ADD_ZIP" => $order->delivery['postcode'],
-			"TRANSACTION_IS_AUTH" => ""
+		"MERCHANT_NUMBER" => MODULE_PAYMENT_PAYHUB_ORGID,
+		"USER_NAME" => MODULE_PAYMENT_PAYHUB_USERNAME,
+		"PASSWORD" => MODULE_PAYMENT_PAYHUB_PASSWORD,
+		"TERMINAL_NUMBER" => MODULE_PAYMENT_PAYHUB_TERMID,
+		"TRANSACTION_CODE" => "01",
+		"ACCOUNT_DATA_SOURCE" => "T",
+		"CUSTOMER_DATA_FIELD" => $_POST['cc_number'],
+		"CARD_EXPIRY_DATE" => $_POST['cc_expires'],
+		"TRANSACTION_NOTE" => "IP: " . zen_get_ip_address() . "Purchased from: " . STORE_NAME . ", Invoice ID: " . $new_order_id, 
+		"CVV_DATA" => $_POST['cc_cvv'],
+		"CVV_CODE" => ($_POST['cc_cvv'] != NULL) ? "Y" : "N",
+		"TRANSACTION_AMOUNT" => (number_format($order->info['total'], 2) * 100),
+		"OFFLINE_APPROVAL_CODE" => "",
+		"TRANSACTION_ID" => "",
+		"CUSTOMER_ID" => "",
+		"CUSTOMER_FIRST_NAME" => $order->billing['firstname'],
+		"CUSTOMER_LAST_NAME" => $order->billing['lastname'],
+		"CUSTOMER_COMPANY_NAME" => $order->billing['company'],
+		"CUSTOMER_JOB_TITLE" => "",
+		"CUSTOMER_EMAIL_ID" => $order->customer['email_address'],
+		"CUSTOMER_WEB" => "",
+		"CUSTOMER_PHONE_NUMBER" => preg_replace('/[^0-9]/', '', $order->customer['telephone']),
+		"CUSTOMER_PHONE_EXT" => "",
+		"CUSTOMER_PHONE_TYPE" => "",
+		"CUSTOMER_BILLING_ADDRESS1" => $order->billing['street_address'],
+		"CUSTOMER_BILLING_ADDRESS2" => "",
+		"CUSTOMER_BILLING_ADD_CITY" => $order->billing['city'],
+		"CUSTOMER_BILLING_ADD_STATE" => $state,
+		"CUSTOMER_BILLING_ADD_ZIP" => $order->billing['postcode'],
+		"CUSTOMER_SHIPPING_ADD_NAME" => $order->delivery['firstname'] . $order->delivery['lastname'],
+		"CUSTOMER_SHIPPING_ADDRESS1" => $order->delivery['street_address'],
+		"CUSTOMER_SHIPPING_ADDRESS2" => "",
+		"CUSTOMER_SHIPPING_ADD_CITY" => $order->delivery['city'],
+		"CUSTOMER_SHIPPING_ADD_STATE" => $state,
+		"CUSTOMER_SHIPPING_ADD_ZIP" => $order->delivery['postcode'],
+		"TRANSACTION_IS_AUTH" => ""
     );
 
-		unset($response);
+	unset($response);
 
     $data_to_send = json_encode($data);
 
@@ -306,7 +308,6 @@ class payhub{
                     CURLOPT_VERBOSE => 0,
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => true,
-                    CURLOPT_CAINFO => "",
                     CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_POST => true,
@@ -329,9 +330,7 @@ class payhub{
     $db_authorization_type = $response->APPROVAL_CODE;
     $db_session_id = zen_session_id();
 
-     $db->Execute("insert into " . TABLE_AUTHORIZENET . "  (id, customer_id,order_id, response_code, response_text, authorization_type, transaction_id, sent, received, time, session_id) values ('', '" . $_SESSION['customer_id'] . "', '" . $new_order_id . "', '" . $db_response_code . "', '" . $db_response_text . "', '" . $db_authorization_type . "', '" . $db_transaction_id . "', '" . print_r($reportable_submit_data, true) . "', '" . $response_list . "', '" . $order_time . "', '" . $db_session_id . "')");
-    
-
+    #$db->Execute("insert into " . TABLE_AUTHORIZENET . "  (id, customer_id,order_id, response_code, response_text, authorization_type, transaction_id, sent, received, time, session_id) values ('', '" . $_SESSION['customer_id'] . "', '" . $new_order_id . "', '" . $db_response_code . "', '" . $db_response_text . "', '" . $db_authorization_type . "', '" . $db_transaction_id . "', '" . print_r($reportable_submit_data, true) . "', '" . $response_list . "', '" . $order_time . "', '" . $db_session_id . "')");
 
     if ($response->RESPONSE_CODE != '00') {
       $messageStack->add_session('checkout_payment', "Payment failed due to the following: " . $response->RESPONSE_CODE . " / " . $response->RESPONSE_TEXT, 'error');
@@ -375,8 +374,8 @@ class payhub{
     global $db;
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable PayHub Module', 'MODULE_PAYMENT_PAYHUB_STATUS', 'True', 'Do you want to accept Credit Card payments via PayHub?', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Organization ID', 'MODULE_PAYMENT_PAYHUB_ORGID', '00000', 'This is your Organization ID', '6', '0', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('API Username', 'MODULE_PAYMENT_PAYHUB_USERNAME', 'api username', 'This your API Username', '6', '0', now())");
-    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('API Password', 'MODULE_PAYMENT_PAYHUB_PASSWORD', 'api password', 'This is your API Password', '6', '0', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('API Username', 'MODULE_PAYMENT_PAYHUB_USERNAME', 'api_username', 'This your API Username', '6', '0', now())");
+    $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('API Password', 'MODULE_PAYMENT_PAYHUB_PASSWORD', 'api_password', 'This is your API Password', '6', '0', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Request CVV Number', 'MODULE_PAYMENT_PAYHUB_USE_CVV', 'True', 'Do you want to ask the customer for the card\'s CVV number', '6', '0', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('PayHub URL', 'MODULE_PAYMENT_PAYHUB_URL', 'https://vtp1.payhub.com/payhubvtws/transaction.json', 'The url to use for processing.', '6', '0', now())");
     $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Terminal ID', 'MODULE_PAYMENT_PAYHUB_TERMID', '000', 'This is your terminal ID', '6', '0', now())");
